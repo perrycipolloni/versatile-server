9660191f6187cb6816f1b26ee5a1cbf97ffdb875
Dylan Nirvana home <dylannirvanahome@Dylans-MacBook-Pro.local>
2020-05-07 09:16:33 -0400

    adds build

---
+++ client/build/precache-manifest.a61657b1a60be32a10540694f1ada0a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b054854a44a27d0541eebfc0acc20fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "760dee16aa78047cbb60",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1dbb95fd152d98e73b52",
+    "url": "/static/css/main.d41364f5.chunk.css"
+  },
+  {
+    "revision": "760dee16aa78047cbb60",
+    "url": "/static/js/2.bc7afe2f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bc7afe2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbb95fd152d98e73b52",
+    "url": "/static/js/main.53e9454a.chunk.js"
+  },
+  {
+    "revision": "bbba16751c6a78c51a11",
+    "url": "/static/js/runtime-main.9c815b4f.js"
+  },
+  {
+    "revision": "a1707a805f8a74ef8d14a848ebb89d21",
+    "url": "/static/media/tone150.a1707a80.png"
+  }
+]);
